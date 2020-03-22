542ff8bece46301268e2cda2a4a4c5102d653d54
marcelosasamoto <marcelosasamoto@gmail.com>
2020-03-22 02:12:28 -0300

    0.1

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,7 @@ const UserController = require('./controllers/UserController');
 
 routes.get("/user", UserController.index);
 routes.post("/user",UserController.store);
+routes.get("/user/:id",UserController.show);
+routes.put("/user/:id",UserController.update);
+routes.delete("/user/:id",UserController.destroy);
 module.exports  = routes;
